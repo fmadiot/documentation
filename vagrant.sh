e7e6deb1329ba3797b1714c8d53d6f5b6cc58a54
Mélanie Bats <melanie.bats@obeo.fr>
2014-06-04 14:48:04 +0200

    Fix issue on jekyll install

--- vagrant.sh
+++ vagrant.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 ruby --version
 gem --version
-sudo apt-get -y install build-essential && sudo gem install rdiscount --no-ri --no-rdoc
-sudo gem install jekyll --version '= 1.5.1' --no-ri --no-rdoc
+sudo gem install jekyll --version '= 1.5.1'
 jekyll build
 
