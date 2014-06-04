0f7bfdca8170a520d78c84b63723d7ee5057198a
Mélanie Bats <melanie.bats@obeo.fr>
2014-06-04 14:40:51 +0200

    Fix issue on ruby gem install

--- vagrant.sh
+++ vagrant.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-
-sudo apt-get -y install build-essential && sudo /opt/vagrant_ruby/bin/gem install rdiscount --no-ri --no-rdoc
-sudo /opt/vagrant_ruby/bin/gem install jekyll --version '= 1.5.1' --no-ri --no-rdoc
+ruby --version
+gem --version
+sudo apt-get -y install build-essential && sudo gem install rdiscount --no-ri --no-rdoc
+sudo gem install jekyll --version '= 1.5.1' --no-ri --no-rdoc
 jekyll build
 
