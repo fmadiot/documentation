034a5aa89d29f287d705bbb090bc5e96055a2372
Mélanie Bats <melanie.bats@obeo.fr>
2014-06-05 09:46:38 +0200

    Fix gh-pages branch switch

--- travis-promote.sh
+++ travis-promote.sh
@@ -1,16 +1,16 @@
 echo "Build $TRAVIS_JOB_NUMBER"
 echo "Git: $TRAVIS_COMMIT [$TRAVIS_BRANCH]"
-echo "List all available branches"
-git branch -v -a
-echo "Checkout gh-pages branch"
-git checkout gh-pages
+mkdir temp
+cd temp
+echo "Clone documentation repo and heckout gh-pages branch"
+git clone https://$GITHUB_TOKEN@github.com/UML-Designer/documentation.git -b gh-pages --quiet
 echo "Remove existing documentation web site old content"
 find . | grep -v ".git\|toc.xml\|_site" | xargs rm -r
 ls
 echo "Move new documentation web site content to root"
-mv _site/* .
+mv ../../_site/* .
 echo "Remove generated site repository"
-rm -r _site
+rm -r ../../_site
 echo "gh-pages branch root content"
 ls
 echo "Push new documentation web site content on gh-pages branch"
